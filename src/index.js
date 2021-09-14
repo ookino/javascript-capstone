a2d9b7572b667e360b78cca16055bfe6c65f5d33
Yaseer Anaisi Okino <37089367+yaseerokino@users.noreply.github.com>
2021-09-14 13:47:04 +0100

    Add function to render meal items

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './style/style.css';
+
+import API from './modules/data';
+import renderMeals from './modules/render';
+
+const endpoint = 'https://www.themealdb.com/api/json/v1/1/';
+
+const displayMeals = () => {
+  API.getMeals(`${endpoint}search.php?f=b`).then((mealsData) => {
+    renderMeals(mealsData);
+  });
+};
+
+window.addEventListener('load', displayMeals);
---
+++ src/modules/render.js
@@ -0,0 +1,35 @@
+const ul = document.getElementById('meal-list');
+const renderMeals = (mealsData) => {
+  console.log(mealsData);
+  const { meals } = mealsData;
+  console.log(meals);
+
+  meals.forEach((mealItem) => {
+    const li = document.createElement('li');
+    li.className = 'meal-item';
+    const img = document.createElement('img');
+    img.src = mealItem.strMealThumb;
+    img.className = 'meal-image';
+    li.appendChild(img);
+    const div = document.createElement('div');
+    div.className = 'meal-meta';
+    li.appendChild(div);
+    const p = document.createElement('p');
+    p.className = 'meal-title';
+    p.innerText = mealItem.strMeal;
+    div.appendChild(p);
+    const likeBtn = document.createElement('button');
+    likeBtn.className = 'icn-btn';
+    div.appendChild(likeBtn);
+    const likeIcon = document.createElement('i');
+    likeIcon.className = 'far fa-heart';
+    likeBtn.appendChild(likeIcon);
+    const commentBtn = document.createElement('button');
+    commentBtn.className = 'btn';
+    commentBtn.innerText = 'Comment';
+    li.appendChild(commentBtn);
+    ul.appendChild(li);
+  });
+};
+
+export default renderMeals;
