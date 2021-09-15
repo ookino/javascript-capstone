f8dbbc73d3c96af050df76b21dcfd56760462a28
Yaseer Anaisi Okino <37089367+yaseerokino@users.noreply.github.com>
2021-09-15 14:41:07 +0100

    Add function to render number of likes

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import API from './modules/data';
 import renderMeals from './modules/render';
 
 const endpoint = 'https://www.themealdb.com/api/json/v1/1/';
+const likesEndpoint =
+  'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/lvQFdeSbvijlsDX1Y4VY/likes/';
 
 const displayMeals = () => {
-  API.getMeals(`${endpoint}search.php?f=b`).then((mealsData) => {
-    renderMeals(mealsData);
+  Promise.all([
+    API.getMeals(`${endpoint}search.php?f=b`),
+    API.getLikes(likesEndpoint),
+  ]).then((data) => {
+    renderMeals(data[0], data[1]);
   });
 };
 
