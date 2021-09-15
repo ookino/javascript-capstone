5112ea9094c463f516f0437d218af1ae2b86da6e
Yaseer Anaisi Okino <37089367+yaseerokino@users.noreply.github.com>
2021-09-15 14:40:44 +0100

    Add function to get all likes

--- src/modules/data.js
+++ src/modules/data.js
@@ -8,6 +8,21 @@ const API = {
       return data;
     }
   },
+  getLikes: async () => {
+    const res = await fetch(
+      'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/lvQFdeSbvijlsDX1Y4VY/likes/',
+      { method: 'GET' }
+    );
+    if (!res.ok) {
+      throw new Error(`API error! status: ${res.status}`);
+    } else {
+      const data = await res.json();
+      return data;
+    }
+  },
+
+  getComments: async () => {},
+  getReservations: async () => {},
 };
 
 export default API;
