955dfac67810d97cb0cb6b6e827b9f703fa2164d
Yaseer Anaisi Okino <37089367+yaseerokino@users.noreply.github.com>
2021-09-16 15:20:23 +0100

    Add function to post a new like to the invovement api

--- src/modules/data.js
+++ src/modules/data.js
@@ -8,10 +8,11 @@ const API = {
       return data;
     }
   },
+
   getLikes: async () => {
     const res = await fetch(
       'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/lvQFdeSbvijlsDX1Y4VY/likes/',
-      { method: 'GET' },
+      { method: 'GET' }
     );
     if (!res.ok) {
       throw new Error(`API error! status: ${res.status}`);
@@ -20,11 +21,31 @@ const API = {
       return data;
     }
   },
+  postLike: async (itemId) => {
+    const res = await fetch(
+      'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/lvQFdeSbvijlsDX1Y4VY/likes/',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          item_id: itemId,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      }
+    );
+    if (!res.ok) {
+      throw new Error(`API error! status: ${res.status}`);
+    } else {
+      const data = await res.text();
+      console.log('res => ', data);
+    }
+  },
 
   getComments: async () => {
     const res = await fetch(
       'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/comments/',
-      { method: 'GET' },
+      { method: 'GET' }
     );
     if (!res.ok) {
       throw new Error(`API error! status: ${res.status}`);
@@ -36,7 +57,7 @@ const API = {
   getReservations: async () => {
     const res = await fetch(
       'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/reservations/',
-      { method: 'GET' },
+      { method: 'GET' }
     );
     if (!res.ok) {
       throw new Error(`API error! status: ${res.status}`);
