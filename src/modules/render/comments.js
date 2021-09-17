0ca1cdb7ebb6fafaa08e00ca282630f328a5bbf0
Kossi-stack <81444338+Kossi-stack@users.noreply.github.com>
2021-09-17 10:59:55 -0700

    Add comments counter to comments.js file

--- src/modules/render/comments.js
+++ src/modules/render/comments.js
@@ -1,4 +1,5 @@
 import API from '../data';
+import counter from '../counter'
 
 const modal = document.getElementById('modal-comment');
 
@@ -69,6 +70,8 @@ const commentModal = (meal) => {
     if (data === 'No comments available for this meal') {
       commentUl.innerHTML = `<li class="no-comments">${`${data}. Add a new comment`}</li>`;
     } else {
+      let commentCounter = counter.comments(data);
+      allCommentsTitle.innerText = `All Comments(${commentCounter})`;
       data.forEach((userComment) => {
         displayComments(commentUl, userComment);
       });
